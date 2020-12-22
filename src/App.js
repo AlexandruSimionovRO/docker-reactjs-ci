7b154f3cefecaa101939b168e8f162eac6aa7609
Alexandru Simionov <alexandru.simionov.ro@gmail.com>
2020-12-22 20:42:56 +0000

    Changed App Text!

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">uDevOps2020</h1>
         <p>
-          New use of Docker Compose.
+          I was changed on the Feature branch.
         </p>
         <a
           className="App-link"
